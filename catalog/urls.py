c4e1b6d0cc6a75dc343d9b0752cea1c8c4cdb5ae
Dmitriy Bychkov <582620@gmail.com>
2023-08-02 17:28:38 +0800

    add a link for the new app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+
 from catalog.views import index, contacts
+from contacts import views
 
 urlpatterns = [
     path('', index),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('contacts/', views.contact_view, name='contacts'),
 ]
