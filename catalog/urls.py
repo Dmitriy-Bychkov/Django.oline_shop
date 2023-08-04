148a688489f430709f89cb4b30febd725bb6f25f
Dmitriy Bychkov <582620@gmail.com>
2023-08-04 13:02:22 +0800

    Write an url address for the home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-
-from catalog.views import index, contacts
+from catalog.views import index, contacts, home
 from contacts import views
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
     path('contacts/', views.contact_view, name='contacts'),
+    path('products/<int:product_id>/', home),
 ]
