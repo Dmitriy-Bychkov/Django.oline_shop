4ebe5b66718255460c64216636109e048ee7dadf
Dmitriy Bychkov <582620@gmail.com>
2023-08-09 14:22:43 +0800

    Rewrite Catalog app controllers to CBV-form

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 from contacts.models import Contact
 
 
-def contact_view(request):
-    """Контроллер для страницы с контактной информацией"""
+# def contact_view(request):
+#     """Контроллер для страницы с контактной информацией"""
+#
+#     contacts = Contact.objects.all()
+#     return render(request, 'contact_detail.html', {'contacts': contacts})
 
-    contacts = Contact.objects.all()
-    return render(request, 'contacts.html', {'contacts': contacts})
+class ContactDetailView(DetailView):
+    model = Contact
