81535c73d0f6ee31c9abc70c3bb0c72377fd6199
Dmitriy Bychkov <582620@gmail.com>
2023-08-10 08:17:33 +0800

    Add the description for the classes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-
 from catalog.models import Product, Category
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """Представление раздела - продуктов в админке"""
+
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description',)
@@ -12,4 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """Представление раздела - категорий в админке"""
+
     list_display = ('id', 'name')
