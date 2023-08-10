f9e34ac8d4066e74d1bcdaa8df6dffebe1f8eb30
Dmitriy Bychkov <582620@gmail.com>
2023-08-10 08:28:20 +0800

    Rewrite global URLs

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path('contacts/', include('contacts.urls')),
+    path('', include('contacts.urls')),
     path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
