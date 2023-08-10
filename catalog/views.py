f3d64b5d334d21f0ef43bedb2d8b815fc66c7122
Dmitriy Bychkov <582620@gmail.com>
2023-08-10 08:29:57 +0800

    Remove extra lines

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,5 @@
 from django.views.generic import DetailView, ListView, TemplateView
-
 from catalog.models import Product
-from contacts.models import Contact
 
 
 class ProductListView(ListView):
@@ -17,22 +15,3 @@ class ProductDetailView(DetailView):
     """Контроллер для страницы с описанием товара"""
 
     model = Product
-
-
-# def contacts(request):
-#     """Контроллер для страницы с контактной информацией"""
-#
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'Имя клиента: {name}\n'
-#               f'Телефон: {phone}\n'
-#               f'Сообщение: {message}')
-#
-#     contacts = Contact.objects.all()
-#
-#     return render(request, 'catalog/contact_detail.html', {'contacts': contacts})
-
-# class ContactView(TemplateView):
-#     template_name = 'catalog/contact_detail.html'
