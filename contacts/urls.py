a9a9dd9f0aebdfef41681a638eccabfbe4fef687
Dmitriy Bychkov <582620@gmail.com>
2023-08-10 08:28:41 +0800

    Delete extra lines

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from contacts.views import ContactDetailView
+from contacts.views import ContactListView
 from contacts.apps import ContactsConfig
+
 app_name = ContactsConfig.name
 
 urlpatterns = [
-    path('<int:pk>', ContactDetailView.as_view()),
-    #path('contacts/', views.contact_view, name='contacts'),
-    #path('products/<int:pk>/', ProductDetailView.as_view(), name='view'),
+    path('contacts/', ContactListView.as_view(), name='contacts'),
 ]
