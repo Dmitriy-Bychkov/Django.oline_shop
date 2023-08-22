ac291fb345b4ff10c190e2dbeb1d808ca42b02c6
Dmitriy Bychkov <582620@gmail.com>
2023-08-22 11:02:04 +0800

    Add new Version model and make migrations for it

--- catalog/models.py
+++ catalog/models.py
@@ -39,3 +39,21 @@ class Category(models.Model):
         verbose_name = "категория"
         verbose_name_plural = "категории"
         ordering = ('name',)
+
+
+class Version(models.Model):
+    """Создание модели - полей версии продукта в таблице БД"""
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+    version_number = models.CharField(max_length=100, verbose_name='номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='название версии')
+    is_current_version = models.BooleanField(default=False, verbose_name='признак текущей версии')
+
+    def __str__(self):
+        return f'{self.version_name}, {self.version_number}'
+
+    class Meta:
+        """Представление написания заголовков в админке"""
+
+        verbose_name = "версия"
+        verbose_name_plural = "версии"
