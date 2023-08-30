73d4f89f1bf0317a45ee160decc601c086499065
Dmitriy Bychkov <582620@gmail.com>
2023-08-30 23:18:12 +0800

    Remove unnecessary fields and add max_length parameter

--- users/models.py
+++ users/models.py
@@ -8,14 +8,11 @@ class User(AbstractUser):
     """Модель для пользователя"""
 
     username = None
+
     email = models.EmailField(unique=True, verbose_name='почта')
-
-    email_is_confirmed = models.BooleanField(default=False, verbose_name='подтверждено')
-    email_confirm_key = models.CharField(max_length=30, verbose_name='код подтверждения почты', **NULLABLE)
-
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
-    country = models.CharField(**NULLABLE, verbose_name='страна')
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
