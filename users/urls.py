9edef63c2695fc430b1ecc7724653e7c0fa6e0fc
Dmitriy Bychkov <582620@gmail.com>
2023-08-30 23:22:06 +0800

    Add urls for users app pages

--- users/urls.py
+++ users/urls.py
@@ -1,21 +1,19 @@
-# from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
-# from django.urls import path
-#
-# from users.apps import UsersConfig
-# from users.views import RegisterView, ProfileView, UserDetailView, confirm_email, activate_email, password_reset
-# from utils.utils import generate_password
-#
-# app_name = UsersConfig.name
-#
-# urlpatterns = [
-#     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
-#     path('register/', RegisterView.as_view(), name='register'),
-#     path('profile', ProfileView.as_view(), name='profile'),
-#     path('profile/view', UserDetailView.as_view(), name='profile_view'),
-#     path("profile/generate_password", generate_password, name="generate_password_profile"),
-#     path('confirm', confirm_email, name='confirm'),
-#     path('activate<str:key>', activate_email, name='activate'),
-#     path('password_reset/', password_reset, name='password_reset'),
-#
-# ]
+from django.urls import path
+from users.views import LoginView, LogoutView, RegisterView, UserUpdateView, generate_password, \
+    UserConfirmEmailView, \
+    UserConfirmedView, password_reset, UserConfirmationSentView
+
+app_name = "users"
+
+urlpatterns = [
+
+    path("", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("registration/", RegisterView.as_view(), name="registration"),
+    path('email_confirmation_sent/', UserConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('confirm_email/<str:uidb64>/<str:token>/', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email_confirmed/', UserConfirmedView.as_view(), name='email_confirmed'),
+    path("profile/", UserUpdateView.as_view(), name="profile"),
+    path("profile/generate_password", generate_password, name="generate_password"),
+    path('password_reset/', password_reset, name='password_reset'),
+]
