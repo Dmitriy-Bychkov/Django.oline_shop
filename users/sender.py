dbf968e432134b7b4fd73b24f09f0ba89a2371a9
Dmitriy Bychkov <582620@gmail.com>
2023-08-30 23:21:03 +0800

    Add function for sending emails

---
+++ users/sender.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def mail_sender(to, theme, message):
+    """Функция по отправке сообщений пользователю"""
+
+    file_content = message
+
+    msg = MIMEText(file_content)
+    msg['Subject'] = theme
+    msg['From'] = 'Dm1tr1y11@yandex.ru'
+    msg['To'] = to
+
+    smtp_server = 'smtp.yandex.ru'
+    smtp_port = 465
+    smtp_username = 'Dm1tr1y11@yandex.ru'
+    smtp_password = 'h!W(S@m1VDLi'
+
+    s = smtplib.SMTP_SSL(smtp_server, smtp_port)
+    s.login(smtp_username, smtp_password)
+    s.sendmail('Dm1tr1y11@yandex.ru', [to], msg.as_string())
+
+    s.quit()
