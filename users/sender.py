2d7dc99e5847077657d85d5c5d61030538cc4d55
Dmitriy Bychkov <582620@gmail.com>
2023-08-31 12:37:11 +0800

    Add application password

--- users/sender.py
+++ users/sender.py
@@ -15,7 +15,8 @@ def mail_sender(to, theme, message):
     smtp_server = 'smtp.yandex.ru'
     smtp_port = 465
     smtp_username = 'Dm1tr1y11@yandex.ru'
-    smtp_password = 'h!W(S@m1VDLi'
+    # smtp_password = 'h!W(S@m1VDLi'  # обычный пароль
+    smtp_password = 'gqwfrlxxruwacfgz'  # пароль приложения яндекс
 
     s = smtplib.SMTP_SSL(smtp_server, smtp_port)
     s.login(smtp_username, smtp_password)
