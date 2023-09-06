f9e2df8985e17a9d5dff8a96be659aa544954f9f
Dmitriy Bychkov <582620@gmail.com>
2023-09-06 16:54:19 +0800

    Add PasswordForm for the form to set new user's password

--- users/forms.py
+++ users/forms.py
@@ -19,3 +19,18 @@ class UserProfileForm(StyleFormMixin, UserChangeForm):
         super().__init__(*args, **kwargs)
 
         self.fields['password'].widget = forms.HiddenInput()
+
+
+class PasswordForm(StyleFormMixin, forms.Form):
+    new_password = forms.CharField(label='Новый пароль', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password and confirm_password and new_password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        return cleaned_data
